8d3128723cbe487a30fdf544b0d66b0a78864092
kevinvsequeira <kevinvsequeira@instance-1.us-central1-a.c.molten-precinct-244414.internal>
2019-12-18 19:16:54 +0000

    toLis 13:16AM

---
+++ src/Graficos/ArbolBB.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Graficos;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.LinkedList;
+import javax.swing.JPanel;
+
+
+public class ArbolBB {
+
+    private Nodo raiz;
+    int num_nodos;
+    int alt;
+
+    public ArbolBB() {
+        raiz = null;
+    }
+    
+    public boolean agregar(int dato) {
+        Nodo nuevo = new Nodo(dato, null, null);
+        insertar(nuevo, raiz);
+        return true;
+    }
+    
+    //Metodo para insertar un dato en el arbol...no acepta repetidos :)
+    public void insertar(Nodo nuevo, Nodo pivote) {
+        if (this.raiz == null) {
+            raiz = nuevo;
+        } else {
+            if (nuevo.getDato() <= pivote.getDato()) {
+                if (pivote.getIzq() == null) {
+                    pivote.setIzq(nuevo);
+                } else {
+                    insertar(nuevo, pivote.getIzq());
+                }
+            } else {
+                if (pivote.getDer() == null) {
+                    pivote.setDer(nuevo);
+                } else {
+                    insertar(nuevo, pivote.getDer());
+                }
+            }
+        }
+
+    }
+
+    public Nodo getRaiz() {
+        return raiz;
+    }
+
+    public void setRaiz(Nodo raiz) {
+        this.raiz = raiz;
+    }
+
+    //Recorrido preorden, recibe el nodo a empezar (raiz) y una linkedlist para ir guardando el recorrido
+    public LinkedList preOrden() {
+        LinkedList rec = new LinkedList();
+        preorden(raiz, rec);
+        return rec;
+    }
+    
+    public void preorden(Nodo aux, LinkedList recorrido) {
+        if (aux != null) {
+            recorrido.add(aux.getDato());
+            preorden(aux.getIzq(), recorrido);
+            preorden(aux.getDer(), recorrido);
+        }
+    }
+
+    //Recorrido inorden, recibe el nodo a empezar (raiz) y una linkedlist para ir guardando el recorrido
+    public LinkedList inOrden() {
+        LinkedList rec = new LinkedList();
+        inorden(raiz, rec);
+        return rec;
+    }
+    
+    public void inorden(Nodo aux, LinkedList recorrido) {
+        if (aux != null) {
+            inorden(aux.getIzq(), recorrido);
+            recorrido.add(aux.getDato());
+            inorden(aux.getDer(), recorrido);
+        }
+    }
+
+    //Recorrido postorden, recibe el nodo a empezar (raiz) y una linkedlist para ir guardando el recorrido
+    public LinkedList postOrden() {
+        LinkedList rec = new LinkedList();
+        postorden(raiz, rec);
+        return rec;
+    }
+    public void postorden(Nodo aux, LinkedList recorrido) {
+        if (aux != null) {
+            postorden(aux.getIzq(), recorrido);
+            postorden(aux.getDer(), recorrido);
+            recorrido.add(aux.getDato());
+        }
+    }
+
+    //Metodo para verificar si hay un nodo en el arbol
+    public boolean existe(int dato) {
+        Nodo aux = raiz;
+        while (aux != null) {
+            if (dato == aux.getDato()) {
+                return true;
+            } else if (dato > aux.getDato()) {
+                aux = aux.getDer();
+            } else {
+                aux = aux.getIzq();
+            }
+        }
+        return false;
+    }
+
+    private void altura(Nodo aux, int nivel) {
+        if (aux != null) {
+            altura(aux.getIzq(), nivel + 1);
+            alt = nivel;
+            altura(aux.getDer(), nivel + 1);
+        }
+    }
+
+    //Devuleve la altura del arbol
+    public int getAltura() {
+        altura(raiz, 1);
+        return alt;
+    }
+    
+     public JPanel getdibujo() {
+        return new ArbolExpresionGrafico(this);
+    }
+     
+    public ResultSet consultaCliente() {
+        String[] resul = new String[8];
+        ResultSet myRs = null;
+        try {
+            Principal.DBConnection db = new Principal.DBConnection();
+            java.sql.Connection conn = db.connect();
+            Statement myStmt = conn.createStatement();
+            String sql = "SELECT * from clientes";
+//            System.out.println("SQL: "+sql);
+            myRs = myStmt.executeQuery(sql);
+
+            if (myRs.next() == false) {
+//                System.out.println("ResultSet in empty in Java");
+                return null;
+            } else {
+//                System.out.println("Ced: " + myRs.getString("Cedula"));
+                return myRs;
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        return myRs;
+    }
+}
